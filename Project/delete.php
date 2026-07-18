65b5669b95d71ffee6547adfa67d3320a1899dc3
VishalProject05 <vishalkushwaha7451@gmail.com>
2026-07-18 22:37:22 +0530

    Delete Project directory

--- Project/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $id = $_REQUEST['id'];
-    $con = mysqli_connect("localhost", "root","","dets_db");
-    $qry = "delete from tblexpence where id = $id";
-    mysqli_query($con, $qry);
-    header("location:manageexpense.php");
-?>
--- Project/header.php
+++
@@ -1,1 +0,0 @@
-<h2>Daily Expense Tracking System (Welcome <?php echo $_SESSION['name']; ?>)</h2>
